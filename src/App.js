48d759a1bf4f2dd5c839c87b3d6b485923113f93
Lord-Sekke <86236909+Lord-Sekke@users.noreply.github.com>
2024-06-25 16:23:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           </b>
         </p>
         <p>
-        Your React application is successfully running on <b>Amazon ECS</b> 🚀🔥
+        Version 2 of your React application is successfully running on <b>Amazon ECS</b> 🚀🔥
         </p>
       </header>
     </div>
